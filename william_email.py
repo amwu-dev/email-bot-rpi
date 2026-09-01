271e46ba3b1139815928a7dcbd16174992206749
Andrew Wu <awu12749@gmail.com>
2026-08-31 19:20:56 -0500

    final iteration, raspberry pi draft works

--- william_email.py
+++ william_email.py
@@ -6,6 +6,7 @@ import time
 import glob
 # Import the email modules we'll need
 from email.message import EmailMessage #anki
+import logging #anki
 
 # Open the plain text file whose name is in textfile for reading.
 msg = EmailMessage()
@@ -14,12 +15,19 @@ msg.set_content("Good Morning!")
 # me == the sender's email address
 # you == the recipient's email address
 
+logging.basicConfig(
+    filename="/home/andrew/email_message/william_email.log",
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(message)s"
+)
+
+logging.info("Cron job started")
 
 msg['Subject'] = f'From William <3'
 msg['From'] = secrets.sender
 msg['To'] = secrets.recepient
 
-images = glob.glob('images/*.png') #anki
+images = glob.glob('/home/andrew/email_message/images/*.png') #anki
 for file in images:
     with open(file, 'rb') as fp:
         img_data = fp.read()
@@ -32,9 +40,11 @@ random.seed()
 
 sleep_time = random.randint(0, 600) #anki
 print("Sleeping for " + str(sleep_time) + " seconds before sending the message...")
+logging.info("Sending message after sleeping for " + str(sleep_time) + " seconds")
 time.sleep(sleep_time) #anki
 print("Sending message...")
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server: 
     smtp_server.login(secrets.sender, secrets.password)
     smtp_server.sendmail(secrets.sender, secrets.recepient, msg.as_string())
     print("Message sent!")
+    logging.info("Finished")
